185379876376974b17ef18b8ba8dd5e189052ec3
Evil Artist <Loc.NguyenXuan99@outlook.com>
2022-06-23 09:08:00 +0700

    Apply css

--- assets/js/stylesetup.js
+++ assets/js/stylesetup.js
@@ -3,15 +3,29 @@ $(() => {
 
     tailwind.config = {
         theme: {
-        extend: {
-            colors: {
-                color1: getComputedStyle(document.body ).getPropertyValue('--color1').trim(),
-                color2: getComputedStyle(document.body ).getPropertyValue('--color2').trim(),
-                color3: getComputedStyle(document.body ).getPropertyValue('--color3').trim(),
-                color4: getComputedStyle(document.body ).getPropertyValue('--color4').trim(),
-                color5: getComputedStyle(document.body ).getPropertyValue('--color5').trim(),
+            extend: {
+                colors: {
+                    color1: getComputedStyle(document.body ).getPropertyValue('--color1').trim(),
+                    color2: getComputedStyle(document.body ).getPropertyValue('--color2').trim(),
+                    color3: getComputedStyle(document.body ).getPropertyValue('--color3').trim(),
+                    color4: getComputedStyle(document.body ).getPropertyValue('--color4').trim(),
+                    color5: getComputedStyle(document.body ).getPropertyValue('--color5').trim(),
+                }
+            },
+            fontSize: {
+                'lg': ['44px', {
+                    lineHeight: '48px',
+                    fontWeight: '700'
+                }],
+                'hl': ['20px', {
+                    lineHeight: '24px',
+                    fontWeight: '400'
+                }]
+            },
+            zIndex: {
+                '1': '1',
+                '2': '2',
             }
-        }
         }
     }
 });
